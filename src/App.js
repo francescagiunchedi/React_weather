391a1111edaf418d56b21c7f28a9390d21ac4e40
francescagiunchedi <39530831+francescagiunchedi@users.noreply.github.com>
2022-05-25 19:02:16 +0200

    update boostrap using npm

--- src/App.js
+++ src/App.js
@@ -5,27 +5,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="body-container">
-      <div className="weather-box">
-        <Search />
+    <div className="container">
+      <div className="body-container">
+        <div className="weather-box">
+          <Search />
 
-        <WeeklyForecast />
+          <WeeklyForecast />
+        </div>
+        <p className="footer">
+          Code by{" "}
+          <a href="https://gentle-ganache-e53c21.netlify.app/" target="_blank">
+            {" "}
+            Francesca Giunchedi
+          </a>{" "}
+          free on
+          <a
+            href="https://github.com/francescagiunchedi/React_weather"
+            target="_blank"
+          >
+            {" "}
+            Github
+          </a>
+        </p>
       </div>
-      <p className="footer">
-        Code by{" "}
-        <a href="https://gentle-ganache-e53c21.netlify.app/" target="_blank">
-          {" "}
-          Francesca Giunchedi
-        </a>{" "}
-        free on
-        <a
-          href="https://github.com/francescagiunchedi/React_weather"
-          target="_blank"
-        >
-          {" "}
-          Github
-        </a>
-      </p>
     </div>
   );
 }
