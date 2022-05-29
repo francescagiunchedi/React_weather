a097e89ba31a285065188524d5c430d3598627cc
francescagiunchedi <39530831+francescagiunchedi@users.noreply.github.com>
2022-05-29 21:54:38 +0200

    less api calls

--- src/Search.js
+++ src/Search.js
@@ -6,48 +6,38 @@ import FormattedDate from "./FormattedDate";
 import "./Search.css";
 
 export default function Search() {
-  const [city, Setcity] = useState("");
   const [weather, Setweather] = useState({});
   const [load, setload] = useState(false);
 
-  function updatecity(event) {
-    Setcity(event.target.value);
-  }
+  let form = (
+    <form className="form-group">
+      <input type="search" placeholder="search city" className="form-control" />
+      <input type="button" value="Search" className="btn btn-primary" />
+    </form>
+  );
 
   function ShowTemp(response) {
     Setweather({
+      city: response.data.name,
       date: new Date(response.data.dt * 1000),
       temperarute: response.data.main.temp,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
     });
+
     setload(true);
   }
 
-  function WeatherCall(event) {
-    event.preventDefault();
-    let key = `4ff86370e362a341be667316345b71fc`;
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
-    axios.get(url).then(ShowTemp);
-  }
-  let form = (
-    <form className="form-group" onSubmit={WeatherCall}>
-      <input
-        type="search"
-        placeholder="search city"
-        onChange={updatecity}
-        className="form-control"
-      />
-      <input type="button" value="Search" className="btn btn-primary" />
-    </form>
-  );
   if (load) {
     return (
       <div>
         {form}
         <div className="info-container">
           <ul>
+            <li>
+              <h1>{weather.city}</h1>
+            </li>
             <li>
               <FormattedDate date={weather.date} />
             </li>
@@ -67,20 +57,15 @@ export default function Search() {
       </div>
     );
   } else {
+    let city = "Milan";
+    const key = `4ed23580ff61d0d4cc91543013f4263b`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+    axios.get(url).then(ShowTemp);
+
     return (
       <div>
         {form}
         <div className="info-container">
-          <ul>
-            <li></li>
-            <li></li>
-          </ul>
-          <ul>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-        <div className="infoTemp">
           <h1>Type a city</h1>
         </div>
       </div>
