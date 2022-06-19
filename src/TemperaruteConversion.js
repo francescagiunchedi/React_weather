f6b9e288166987af2701fe6973eb87aa4ac090a7
francescagiunchedi <39530831+francescagiunchedi@users.noreply.github.com>
2022-06-19 12:35:31 +0200

    took of conversion, working forecast

--- src/TemperaruteConversion.js
+++ src/TemperaruteConversion.js
@@ -4,44 +4,12 @@ import React from "react";
 import "./Search.css";
 
 export default function TemperaruteConversion(props) {
-  const [unit, setunit] = useState("Celsius");
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setunit("Fahrenheit");
-  }
-  function showCelsius(event) {
-    event.preventDefault();
-    setunit("Celsius");
-  }
-  if (unit === "Celsius") {
-    return (
-      <div>
-        <h1>
-          <strong>{Math.round(props.celsius)}</strong>
-        </h1>
-        <span>
-          °C|
-          <a href="/" onClick={showFahrenheit}>
-            °F{" "}
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let Fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <div>
-        <h1>
-          <strong>{Math.round(Fahrenheit)}</strong>
-        </h1>
-        <span>
-          <a href="/" onClick={showCelsius}>
-            °C{" "}
-          </a>
-          | °F{" "}
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>
+        <strong>{Math.round(props.celsius)}</strong>
+      </h1>
+      <span>°C</span>
+    </div>
+  );
 }
